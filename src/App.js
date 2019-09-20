afbf7e1f248bfea12141da983598bb68a2e33a59
CScori <amilanotte1366@gmail.com>
2019-09-20 10:33:07 -0500

    adds router set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "./components/Header.js";
+import { Route } from 'react-router-dom';
+import WelcomePage from "./components/WelcomePage.js";
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route path='/' component={WelcomePage} />
+      <Route path='/characters' component={CharacterCard} />
+      <Route path='/location' component={LocationCard} />
     </main>
   );
 }
