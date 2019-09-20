e35b6c7ea66984d4ccecd85efd10c8fa4c703632
CScori <amilanotte1366@gmail.com>
2019-09-20 10:40:50 -0500

    adds nav paths

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { NavLink } from 'react-router-dom'
 
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+    <Nav>
+      <NavLink to='/'>Home</NavLink>
+      <NavLink to='/characters'>Characters</NavLink>
+      <NavLink to='/location'>Locations</NavLink>
+
+    </Nav>
     </header>
   );
 }
