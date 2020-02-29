c7dce4ea87349f1c10669b9e781167e7691a06e5
CScori <49799016+CScori@users.noreply.github.com>
2020-02-29 15:36:12 -0800

    adds search form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,27 @@
 import React, { useState } from "react";
 
-export default function SearchForm() {
- 
+export default function SearchForm(props) {
+ console.log('search props', props)
+
+ const handleInputChange = e => {
+  props.setQuery(e.target.value);
+};
   return (
     <section className="search-form">
-     // Add a search form here
+       <form >
+      <input
+      id="name" 
+      type="text" 
+      name="textfield" 
+      placeholder="Search by Name"
+      value={props.query} 
+      onChange={handleInputChange}/>
+
+
+
+    <Link to="/"><button>Home
+          </button></Link>
+    </form>
     </section>
   );
 }
