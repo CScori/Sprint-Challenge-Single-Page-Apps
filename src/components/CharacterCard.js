3a11fd899a44df04b0f1d210633cdc233858323a
CScori <49799016+CScori@users.noreply.github.com>
2020-02-29 15:33:37 -0800

    adds get and mapfor characters

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,22 @@
 import React from "react";
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  console.log(props)
+  const {name, status, species, type, gender, image} = props
+  return(
+    <div> 
+      <Card id={props.key}>
+        <CardImg top width="100%" src={image} alt={name} />
+        <CardBody>
+          <CardTitle>{species}</CardTitle>
+          <CardSubtitle>{status}</CardSubtitle>
+          <CardText>{name} is a {male} {species} from {location} who is currently {status}. This  {gender} {type} may be armed and dangerous</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
 }
